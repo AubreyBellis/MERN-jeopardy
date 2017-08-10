34b344ebb979c24a8aa1b355ca39b7afc1cc5718
AubreyBellis <contactaubreybellis@gmail.com>
2017-08-10 14:14:52 -0400

    finished

--- controllers/game.js
+++ controllers/game.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Game = require("../models/game");
-
+const Category = require("../models/category");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -8,6 +8,29 @@ router.get("/", (req, res) => {
     res.json(games);
   });
 });
+//creating a new game
+router.post('/',(req,res) => {
+const newGame = new Game();
+newGame.user = req.body.user;
+newGame.points = 0;
+
+
+//finding categories for new game with for loop and Math.
+category.find().then((categories) => {
+  for (var i = 0; 1 > 6; i ++){
+  const randomNumber = Math.floor(Math.random() = categories.length);
+  const randomCategory= categories[randomNumber];
+  newGame.categories.push(randomCateagory);
+  }
+ return newGame.save
+})
+//saving new game
+.then(() => {
+  return res.json(game);
+})
+console.log(req.body.name);
+});
+
 
 router.get("/:id", (req, res) => {
   Game.findById(req.params.id).then((game) => {
--- server.js
+++ server.js
@@ -1,28 +1,35 @@
-
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const GamesController = require("./controllers/game");
+const CategoriesController = require("./controllers/category");
 const app = express();
+
 mongoose.Promise = global.Promise;
+
 // Mongoose Connection
 mongoose.connect(process.env.MONGODB_URI);
 const connection = mongoose.connection;
+
 connection.on('connected', () => {
-  console.log("LIVE");
+  console.log("Mongoose Connected Successfully");
 })
+
 connection.on('error', (err) => {
   console.log("Mongoose connection error: " + err);
 })
+
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/client/build/'));
 
+app.use("/api/category", CategoriesController);
 app.use("/api/game", GamesController);
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/client/build/index.html')
+  res.sendFile(__dirname + "/client/build/index.html");
 });
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log("We're on bitches!: " + PORT);
+  console.log("App is listening on: " + PORT);
 });
